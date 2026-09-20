a9342b5a897e7b1cd979b08ba4c63482353dd194
JafaryNm <Jaffarymdegela@gmail.com>
2026-09-20 23:40:42 +0300

    feat(Error) typescript error check

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world");
+const students = [
+  {
+    name: "Johnson",
+    score: 30,
+  },
+
+  {
+    name: "Japhet",
+    score: 70,
+  },
+];
+
+for (student of students) {
+  console.log("Name", students.name);
+  console.log("Score", students.score);
+}
